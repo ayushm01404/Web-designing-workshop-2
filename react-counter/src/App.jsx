651785b9355fd82c0824015c7ce47e2aca0d49ef
AYUSH09082007 <ayush.25b15410248@abes.ac.in>
2026-05-16 14:03:11 +0530

    my local changes before oull

--- react-counter/src/App.jsx
+++ react-counter/src/App.jsx
@@ -1,14 +1,15 @@
 import "./index.css";
 import Counter from "./Counter";
 import List from "./List";
+import Form from "./Form";
 
 const App = () => {
 
   return (
-    <>
-    <Counter/>
-    {/* <List/> */}
-    </>
+  
+     </counter>
+   
+    
   );
 };
 
---
+++ react-counter/src/Form.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    
+    if (!formData.name || !formData.email || !formData.password) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    alert("Registration Successful!");
+    console.log(formData);
+  };
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>REGISTRATION FORM</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+        </div>
+
+        <br />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
